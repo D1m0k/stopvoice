03b27ca8a4db9eeb154fa7adc638dd8e205efef5
D1m0k <konovalov.d.s@gmail.com>
2022-10-28 18:28:10 +0300

    1
    1

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3
+RUN pip3 install --no-cache-dir --upgrade pip \
+&& pip3 install --no-cache-dir telethon
+
+WORKDIR /app
+
+COPY . .
+
+
+CMD [ "python ./stopvoice.py" ]
---
+++ stopvoice.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from http import client
+from telethon import TelegramClient, events
+import pathlib
+import os
+
+os.chdir(pathlib.Path(__file__).parent.resolve())
+
+api_id = print(os.environ['ID'])
+api_hash = print(os.environ['HASH'])
+username = print(os.environ['NAME'])
+client = TelegramClient(username, api_id, api_hash)
+
+@client.on(events.NewMessage(incoming = True))
+async def event_handler(event):
+    whitelist = []
+    if (event.chat_id > 0) and (event.chat_id not in whitelist):
+        if (event.message.voice):
+            await event.respond('__Пользователь ограничил функцию голосовых сообщений__')
+            await client.delete_message(client.chat_id, [event.id])
+
+client.start()
+client.run_until_disconnected()
